ad85427e185b377207a2690f12ff3cc2153b1245
ALLCLAD <charongnounkabiti@gmail.com>
2026-08-19 21:31:13 +0000

    Premier commit

---
+++ secretariat_api/src/main/java/com/eglise/secretariat/SecretariatApiApplication.java
@@ -0,0 +1,13 @@
+package com.eglise.secretariat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretariatApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretariatApiApplication.class, args);
+	}
+
+}
---
+++ secretariat_api/src/test/java/com/eglise/secretariat/SecretariatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eglise.secretariat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretariatApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
